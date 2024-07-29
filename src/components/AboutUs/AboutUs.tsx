aa8feb9920cee372152436b7e92583daa9f9b141
Artyomsheq <artimushka@gmail.com>
2024-07-29 22:55:13 +0600

    add AboutUs section

---
+++ src/components/AboutUs/AboutUs.tsx
@@ -0,0 +1,61 @@
+import React from "react";
+import s from "./AboutUs.module.scss";
+import LandingCarousel from "../ui/landingCarousel/LandingCarousel";
+import LandingBtn from "../ui/landingBtn/LandingBtn";
+import Image from "next/image";
+import Container from "../Container/Container";
+import { aboutUsMocks } from "../utils/mocks/aboutUsMocks";
+
+const AboutUs = () => {
+  return (
+    <section className={s.about}>
+      <Container>
+        <LandingCarousel
+          arrows={false}
+          slidesToShow={1}
+          slidesToScroll={1}
+          pagination={true}
+          autoplay={true}
+          autoplaySpeed={3500}
+          infinite={true}
+        >
+          {aboutUsMocks.map((card, index) => (
+            <div
+              key={index}
+              className={`${s.carousel_card} ${
+                index === 0 ? s.first_card : s.other_cards
+              }`}
+            >
+              <div className={s.carousel_card_background}>
+                <Image
+                  src={card.backgroundImage}
+                  width={100}
+                  height={100}
+                  alt=""
+                />
+              </div>
+              <div className={s.carousel_card_left}>
+                <h2>{card.title}</h2>
+                <h3>{card.subtitle}</h3>
+                <p>{card.description}</p>
+                <LandingBtn
+                  className={s.carousel_card_left_btn}
+                  nameBtn={card.buttonText}
+                />
+              </div>
+              <div className={s.carousel_card_right}>
+                <Image src={card.image} width={100} height={100} alt="" />
+                <LandingBtn
+                  className={s.carousel_card_left_btn}
+                  nameBtn={card.buttonText}
+                />
+              </div>
+            </div>
+          ))}
+        </LandingCarousel>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutUs;
---
+++ src/components/utils/mocks/aboutUsMocks.ts
@@ -0,0 +1,37 @@
+export const aboutUsMocks = [
+    {
+      backgroundImage: "/typesWork/background.svg",
+      title: "О нас",
+      subtitle: "Более двадцати лет в зуботехническом искусстве",
+      description:
+        "Зуботехническая лаборатория для нас это нечто большее, чем просто цех для изготовления работ.",
+      buttonText: "Заказать консультацию",
+      image: "/aboutUs/Comand.svg",
+    },
+    {
+      backgroundImage: "/typesWork/background.svg",
+      title: "Техники:",
+      subtitle: "Лихачев Владимир Александрович",
+      description: "Специалист по литью металлов. Стаж работы: 23 года",
+      buttonText: "Заказать консультацию",
+      image: "/aboutUs/aboutUsFirst.svg",
+    },
+    {
+      backgroundImage: "/typesWork/background.svg",
+      title: "Техники:",
+      subtitle: "Пхакадзе Спартак Владимирович",
+      description:
+        "Специалист по выполнению высокоэстетичных работ на своих зубах. Стаж работы - 17 лет ",
+      buttonText: "Заказать консультацию",
+      image: "/aboutUs/aboutUsSecond.svg",
+    },
+    {
+      backgroundImage: "/typesWork/background.svg",
+      title: "Техники:",
+      subtitle: "Мамедов Кенан Иванович",
+      description:
+        "Специалист по выполнению высокоэстетичных работ на своих зубах. Стаж работы - 17 лет",
+      buttonText: "Заказать консультацию",
+      image: "/aboutUs/aboutUsThird.svg",
+    },
+  ];
