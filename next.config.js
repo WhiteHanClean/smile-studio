134141f688da61617e1980f47a93c68fc89e439e
WhiteHanClean <whitehanclean@gmail.com>
2024-08-12 14:49:24 +0600

    end design

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
+  output: "export",
   experimental: {
     appDir: true,
   },
-}
+  exportTrailingSlash: true, // добавьте это, если у вас проблемы с путями
+  sassOptions: {
+    includePaths: [path.join(__dirname, "styles")],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
